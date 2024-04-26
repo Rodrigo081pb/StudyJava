d6176f09f695ee61371230c9742ab9021d3fb242
Rodrigo <kauarodrigo1193@gmail.com>
2024-04-26 01:12:51 -0300

    Adicionando conteudo da parte de anatomia de classes

---
+++ classes/src/Classes.java
@@ -0,0 +1,14 @@
+// Uma classe bem estruturada não quer guerra com ninguém
+
+
+public class Classes {
+
+    // Método principal - main sendo o padrão conforme o abaixo
+    public static void main(String[] args) {
+
+        // Como imprimir uma mensagem
+        System.out.print("Hello world isso é minha palavra dentro de um método");
+
+    }
+
+}
--- docs/1.Módulo/Basic.md
+++ docs/1.Módulo/Basic.md
@@ -1,3 +1,10 @@
 ## Anotações primeiro Moludo
 
 - CRTL + SHIFT + P -> Inicia um novo arquivo
+
+- Algo a ser levado em consideração são os outros curso pra complementar esse já existente tais como: 
+
+-- Java Developer
+-- 
+-- Formação SpringWebFlux
+--
