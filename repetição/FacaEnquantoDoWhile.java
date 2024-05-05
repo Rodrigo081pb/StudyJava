d840401ec94d0dcdd1221c1f2e0527276ea5f6bd
Rodrigo <kauarodrigo1193@gmail.com>
2024-05-04 23:11:40 -0300

    Adicionando comentários para reforçar o entendimento do código

--- repetição/FacaEnquantoDoWhile.java
+++ repetição/FacaEnquantoDoWhile.java
@@ -11,19 +11,35 @@ public class FacaEnquantoDoWhile {
         System.out.println("Ligando...");
 
         do {
-            // Executando repetidas vezes até alguem atender
+            // Inicia um loop do-while. Este tipo de loop garante que o bloco de código seja
+            // executado pelo menos uma vez, mesmo que a condição seja falsa desde o início.
+            // Executando repetidas vezes até alguém atender
 
             System.out.println("Telefone tocando");
+            // A condição do loop do-while é a chamada do método tocando(). Enquanto esse
+            // método retornar true, o loop continuará. Se ele retornar false, o loop
+            // termina.
         } while (tocando());
 
-        System.out.println("Alô !!!");
+        System.out.println("Alô !!!"); // Após o loop, quando alguém atender o telefone, é impresso "Alô !!!".
 
     }
 
+    // Define o método tocando(), que retorna um valor booleano indicando se o
+    // telefone está tocando ou não.
     private static boolean tocando() {
+
+        // Cria um objeto da classe Random para gerar um número aleatório entre 0 e 2.
+        // Se o número gerado for igual a 1, consideramos que alguém atendeu o telefone
+        // (atendeu = true).
         boolean atendeu = new Random().nextInt(3) == 1;
+
+        // Imprime se o telefone foi atendido ou não.
         System.out.println("Atendeu? " + atendeu);
 
+        // Retorna o oposto do valor de atendeu. Ou seja, retorna true se o telefone não
+        // foi atendido nesta chamada, o que faz com que o loop do-while continue, ou
+        // retorna false se alguém atendeu, finalizando o loop.
         return !atendeu;
     }
 
