81e1e4d8738dd5ee9970f196d24023813fe6a52d
Rodrigo <kauarodrigo1193@gmail.com>
2024-05-04 22:23:44 -0300

    Finalizando do while

--- repetição/FacaEnquantoDoWhile.java
+++ repetição/FacaEnquantoDoWhile.java
@@ -1,5 +1,30 @@
+import java.util.Random;
+
 public class FacaEnquantoDoWhile {
     public static void main(String[] args) {
 
+        /*
+         * No exemplo a seguir Rodrigo resolveu ligar para o seu amigo dizendo que hoje
+         * se entupiu de comer tantas besteiras
+         */
+
+        System.out.println("Ligando...");
+
+        do {
+            // Executando repetidas vezes até alguem atender
+
+            System.out.println("Telefone tocando");
+        } while (tocando());
+
+        System.out.println("Alô !!!");
+
     }
+
+    private static boolean tocando() {
+        boolean atendeu = new Random().nextInt(3) == 1;
+        System.out.println("Atendeu? " + atendeu);
+
+        return !atendeu;
+    }
+
 }
