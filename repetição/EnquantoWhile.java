fb197b4649a19be38882a8de2389db9e40adfc40
Rodrigo <kauarodrigo1193@gmail.com>
2024-05-04 22:46:26 -0300

    refatorando o código

--- repetição/EnquantoWhile.java
+++ repetição/EnquantoWhile.java
@@ -1,4 +1,4 @@
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.ThreadLocalRandom; // Importando a classe do pacote para gerar números aleatórios
 
 public class EnquantoWhile {
     public static void main(String[] args) {
@@ -8,20 +8,28 @@ public class EnquantoWhile {
          * 100 ele poderá comprar joias e colocar num guarda-joias
          */
 
-        double mesada = 100;
+        double mesada = 100; // Declaração de váriavel de valor que pedro recebeu de mesada
 
+        // enquanto a mesada for maior que zero
         while (mesada > 0) {
+
+            // chamando o método para gerar um valor aleatório pra o valor da joia
             Double valorJoia = valorAleatorio(); // Vai ser um valor aleatório
 
-            if (valorJoia > mesada) // Se o valor da joia for maior que a mesada o valor da joia vai ser = mesada
+            if (valorJoia > mesada) // se o valor da joia (valorJoia) é maior do que a mesada disponível (mesada).
+                                    // Se for, define o valor da joia igual ao valor da mesada.
                 valorJoia = mesada;
 
-            System.out.println("O valor da joia é: " + valorJoia + "Adicionado no guarda joias");
+            System.out.println("O valor da joia é: " + valorJoia + "Adicionado no guarda joias"); // mprime o valor da
+                                                                                                  // joia gerado e
+                                                                                                  // informa que a joia
+                                                                                                  // foi adicionada ao
+                                                                                                  // guarda-joias.
 
-            mesada = mesada - valorJoia;// A mesada vai ser subtraida pelo valor da joia
+            mesada = mesada - valorJoia;// Subtrai o valor da joia (valorJoia) do valor total da mesada (mesada).
         }
 
-        System.out.println("Mesada: " + mesada); // Aqui vai ser exibido a mesada ao final do código
+        System.out.println("Mesada: " + mesada); // Imprime o valor atual da mesada após a compra da joia
         System.out.println("Pedro gastou toda sua mesada em jóias");
 
     }
@@ -30,3 +38,9 @@ public class EnquantoWhile {
         return ThreadLocalRandom.current().nextDouble(5, 10); // Método para gerar valor entre 5 e 10
     }
 }
+
+// Enquanto ele tiver dinheiro na mesada, ele continuará comprando joias
+// aleatórias até esgotar o saldo. Cada vez que ele compra uma joia, o valor é
+// subtraído da sua mesada. O programa termina quando ele não tiver mais
+// dinheiro. O método valorAleatorio() é utilizado para gerar valores aleatórios
+// para as joias.
