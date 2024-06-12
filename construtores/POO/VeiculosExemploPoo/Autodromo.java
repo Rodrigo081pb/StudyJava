e8e73f51b5dbed71f1923763f543ca77bb5b831a
Rodrigo <kauarodrigo1193@gmail.com>
2024-06-11 23:17:50 -0300

    rolbacks

--- construtores/POO/VeiculosExemploPoo/Autodromo.java
+++ construtores/POO/VeiculosExemploPoo/Autodromo.java
@@ -4,11 +4,15 @@ public class Autodromo {
     public static void main(String[] args) {
         Carro Fiat = new Carro();
         Fiat.setChassi("8742BC");
-        Fiat.ligar();
+        // Fiat.ligar();
 
         Moto honda160fan = new Moto();
         honda160fan.setChassi("7776MTUE");
-        honda160fan.ligar();
+        // honda160fan.ligar();
+
+        Veiculo coringa = Fiat;
+
+        coringa.ligar();
     }
 
 }
